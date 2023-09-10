550ba91941e1cb4958e44d39b409178c6446e72b
Andrew Kerr <kerrand@protonmail.com>
2023-09-10 19:03:51 -0400

    more ch1 exercises

--- ch1.rkt
+++ ch1.rkt
@@ -9,6 +9,9 @@
 (define (divides? a b)
   (= (remainder b a) 0))
 
+(define (average x y) (/ (+ x y) 2))
+(define (reciprocal x) (/ 1 x))
+
 ; Demo - Fibonacci Numbers
 ; Fib(n) = 0 , if n = 0
 ;        = 1 , if n = 1
@@ -275,3 +278,225 @@
         result
         (iter (next a) (op (term a) result))))
   (iter a init))
+
+; using this definition, sum() and product()
+; can be written like so...
+(define (sum-acc term a next b)
+  (accumulate + 0 term a next b))
+
+(define (product-acc term a next b)
+  (accumulate * 1 term a next b))
+
+; 1.33  - accumulate() with a filter
+
+; recursive
+(define (filtered-accumulate op init use? term a next b)
+  (if (> a b)
+      init
+      (op (if (use? a) (term a) init)
+          (filtered-accumulate op init use? term (next a) next b))))
+
+; iterative
+(define (filtered-accumulate-iter op init use? term a next b)
+  (define (iter a result)
+    (cond
+      ((> a b) result)
+      ((use? a) (iter (next a) (op (term a) result)))
+      (else (iter (next a) result))))
+  (iter a init))
+        
+
+; summing sqaures of primes
+(define (sum-square-prime a b)
+  (filtered-accumulate + 0 prime? square a inc b))
+
+; product of positive integers less than n
+; that are relatively prime to n
+(define (rel-prime? a b)
+  (= (gcd a b) 1))
+
+(define (product-relatively-prime n)
+  (define (rel-prime-n? x) (rel-prime? x n))
+  (filtered-accumulate * 1 rel-prime-n? identity 0 inc (dec n)))
+
+; 1.34
+
+; suppose we have a function f that accepts a unary procedure
+; and applies it the value 2...
+(define (f g)
+  (g 2))
+
+; then, the expression (f f) expands to
+; (f 2)
+; (2 2)
+; which results in a type mistach of sorts.
+
+; Demo- Finding fixed points of a function
+(define tolerance 0.000001)
+(define (close-enough? x y)
+  (< (abs (- x y)) tolerance))
+
+(define (fixed-point f first-guess)
+  (define (try guess)
+    (display guess)
+    (newline)
+    (let ((next (f guess)))
+      (if (close-enough? next guess)
+          next
+          (try next))))
+    (try first-guess))
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y)))
+               1.0))
+
+; 1.35 - Golden Ratio
+;
+; The Golden Ratio (1.618033...) is a fixed point
+; of the transformation x -> 1 + 1/x, very cool.
+; (define golden-ratio
+;   (fixed-point (lambda (x) (average x (+ 1 (/ 1 x))))
+;                1.0))
+
+; 1.36 - Solving x^x = 1000
+
+; without average damping
+(fixed-point (lambda (x) (/ (log 1000) (log x)))
+             4)
+
+; with average damping
+(fixed-point (lambda (x) (average x (/ (log 1000) (log x))))
+             4)
+
+; 1.37 - A continued fraction procedure
+
+; recursive
+(define (cont-frac n d k)
+  (define (compute i)
+    (if (> i k)
+        0
+        (/ (n i) (+ (d i) (compute (inc i))))))
+  (compute 1))
+
+; iterative
+(define (cont-frac-iter n d k)
+  (define (iter i result)
+    (if (< i 1)
+        result
+        (iter (dec i) (/ (n i) (+ (d i) result)))))
+  (iter k 0))
+
+; n = 14 is accurate to 4 decimal places, very neat
+(define (golden-ratio n)
+  (reciprocal (cont-frac (lambda (_) 1.0)
+                         (lambda (_) 1.0)
+                         n)))
+
+; 1.38 - e as a continued fraction
+
+(define (e-cf n)
+  (+ (cont-frac (lambda (_) 1.0)
+                (lambda (i)
+                  (if (= (remainder i 3) 2)
+                      (* 2 (+ (floor (/ i 3)) 1))
+                      1))
+                n)
+     2))
+
+; 1.39 - tan(x) as a continued fraction
+;
+; TODO
+
+; Demo - Returning a procedure as a value
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+; Demo - Newton's method
+;
+; If g(x) is a differentiable function, a solution
+; g(x) = 0 will be a fixed point of the function
+; f(x) = x - g(x) / Dg(x), where Dg(x) is the derivative
+; of g evaluated at x.
+
+(define dx 0.00001)
+
+; given a procedure that returns the derivative
+; of a function...
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+; and a procedure that compute the function whose
+; fixed point we need to find in order to compute
+; roots of g via Newton's method...
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+; we can define a procedure to find carry out Newton's
+; method on a function like so
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+; 1.40 - Cubic roots via Newton's Method
+
+(define (cubic a b c)
+  (lambda (x) (+ (cube x)
+                 (* a (square x))
+                 (* b x)
+                 c)))
+
+; e.g. (newtons-method (cubic 1 1 1) 1) ~ -1
+
+; 1.41 - Apply a procedure twice
+(define (twice op) ; op is a unary function
+  (lambda (x) (op (op x))))
+
+; what does (((twice (twice twice)) inc) 5) evaluate to?
+
+; 1.42 - A procedure to compose unary functions
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+; 1.43 - Repeated application
+
+(define (repeated f n)
+  (if (= n 1)
+      f
+      (compose f (repeated f (dec n)))))
+
+; 1.44 - Smoothing a function
+
+(define (smooth f)
+  (lambda (x) (/ (+ (f (- x dx))
+                    (f x)
+                    (f (+ x dx)))
+                 3)))
+
+(define (nth-smoothed f n)
+  ((repeated smooth n) f))
+
+; 1.45 - Nth roots
+
+; 1.46 - Iterated improvement
+
+(define (iterative-improve good-enough? improve)
+  (define (iter guess)
+    (if (good-enough? guess)
+        guess
+        (iter (improve guess))))
+  (lambda (x) (iter x)))
+
+(define (sqrt-iter-gen x)
+  ((iterative-improve
+    (lambda (guess) (close-enough? (square guess) x))
+    (lambda (guess) (average guess (/ x guess))))
+   1.0))
+
+(define (fixed-point-iter-gen f first-guess)
+  ((iterative-improve
+    (lambda (guess) (close-enough? guess (f guess)))
+    f)
+   first-guess))
